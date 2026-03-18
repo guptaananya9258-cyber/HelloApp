c1e3bf2dd28a0c5f1673724ef32a19ed0bd96e4c
ANANYA GUPTA <guptaananya.9258@gmail.com>
2026-03-18 13:56:16 +0530

    Simplify HelloApp to print 'Hello World'

--- HelloApp.java
+++ HelloApp.java
@@ -1,12 +1,5 @@
-public class HelloApp {
-    public static void main(String[] args) {
-        
-        // Check if user has provided a name
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello " + name);
-        } else {
-            System.out.println("Hello User (No name provided)");
-        }
-    }
+public class HelloApp{ 
+    public static void main(String[] args){ 
+        System.out.println("Hello World"); 
+    } 
 }
