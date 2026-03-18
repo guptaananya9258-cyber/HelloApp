d94535853b61b94cb0bc482164530598c54a53d7
ANANYA GUPTA <guptaananya.9258@gmail.com>
2026-03-18 13:54:06 +0530

    Enhance HelloApp to greet user by name

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,12 @@
-public class HelloApp{
-    public static void main(String[] args){
-        System.out.println("Hello World");
+public class HelloApp {
+    public static void main(String[] args) {
+        
+        // Check if user has provided a name
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello " + name);
+        } else {
+            System.out.println("Hello User (No name provided)");
+        }
     }
 }
